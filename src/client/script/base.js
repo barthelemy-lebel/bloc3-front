27b6dff1b61d203c4de818fb411a454d46da3c4f
barthelemy-lebel <barthelemylebel@orange.fr>
2024-06-12 21:44:52 +0200

    Fixed #39 -- Securiser la connexion

--- src/client/script/base.js
+++ src/client/script/base.js
@@ -1,9 +1,9 @@
 function logout() {
   localStorage.clear()
-  window.location.href = '/src/admin/login.html'
+  window.location.href = '/src/client/login.html'
 }
-console.log("test")
+
 if (!localStorage.getItem("jwt-token")){
   alert('Vous devez vous identifier !')
-  window.location.href = '/src/admin/login.html'
+  window.location.href = '/src/client/login.html'
 }
